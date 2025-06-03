e5c5a3dd880ef56d8a0d2c5acad3c52de01c83e0
Rebecca <rebecca.r3@schule.bremen.de>
2025-06-03 13:18:29 +0200

    NHP-5 set up admins

---
+++ src/main/java/de/hitec/nhplus/model/Admin.java
@@ -0,0 +1,25 @@
+package de.hitec.nhplus.model;
+
+import javafx.beans.property.SimpleLongProperty;
+
+public class Admin extends Person {
+
+    private SimpleLongProperty aid;
+    private final String password;
+
+    public Admin(String firstname, String lastname, String password) {
+        super(firstname, lastname);
+        this.password = password;
+    }
+
+    public Admin(long aid, String firstName, String surname, String password) {
+        super(firstName, surname);
+        this.aid = new SimpleLongProperty(aid);
+        this.password =  password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
